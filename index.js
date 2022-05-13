7ebb39a7d2758f2dda669c1092885a2bb9d030b4
InfercielKayn <103080365+InfercielKayn@users.noreply.github.com>
2022-05-12 23:02:12 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+$.fn.getPokemon = function (elemento, pokenumber) {
+    var pokemon = "";
+    $.ajax({
+      url: `https://pokeapi.co/api/v2/pokemon/${pokenumber}`,
+
+      success: function (result) {
+        pokemon = result.sprites.front_shiny;
+        elemento.html("<image src=" + pokemon + ">");
+      }
+    });
+  };
+
+  $("#pokemon_uno_boton").click(function () {
+    $.fn.getPokemon($("#pokemon_uno"), 25);
+  });
+  $("#pokemon_uno_boton").mouseover(function(){
+    $("#pokemon_uno_boton").css("background-color", "yellow");
+  });
